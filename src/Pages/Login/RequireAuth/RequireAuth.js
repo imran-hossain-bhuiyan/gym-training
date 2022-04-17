8c9250961e427b54781349d321962574eef1bf6c
sharifularman <sharifulislamarman20@gmail.com>
2022-04-17 21:22:04 +0600

    register fom done

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useLocation } from 'react-router-dom';
 
-const RequireAuth = () => {
+const RequireAuth = ({children}) => {
+    const [user, loading] =  useAuthState();
+    const location = useLocation();
     return (
         <div>
             <h2>require auth</h2>
